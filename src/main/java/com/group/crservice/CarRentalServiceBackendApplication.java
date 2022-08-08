10f8a831af6857f2ed8359f571c125564895b4f2
midibi-admin <midibi.org@gmail.com>
2022-08-08 22:15:30 +0100

    Initial commit

---
+++ src/main/java/com/group/crservice/CarRentalServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group.crservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/crservice/CarRentalServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.crservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

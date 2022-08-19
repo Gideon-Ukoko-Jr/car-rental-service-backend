2da2607deaff8cd37fd670f2aa562d27c85e2491
midibi-admin <midibi.org@gmail.com>
2022-08-19 21:31:37 +0100

    Minor Edit

--- src/main/java/com/group/crservice/controller/VehicleController.java
+++ src/main/java/com/group/crservice/controller/VehicleController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/vehicles")
-@Api(tags = "User Endpoints", description = "Handles Authentication and User Management")
+@Api(tags = "Vehicle Endpoints", description = "Handles Vehicle Endpoints and Management")
 @AllArgsConstructor
 public class VehicleController {
 

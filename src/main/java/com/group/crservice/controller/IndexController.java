25ea070203f76f51680dccba7ecf188e757f5369
midibi-admin <midibi.org@gmail.com>
2022-08-19 22:05:41 +0100

    Added Index Endpoint

---
+++ src/main/java/com/group/crservice/controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.group.crservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class IndexController {
+
+    @GetMapping(value = "", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String welcomePage() {
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "    <head>\n" +
+                "        <meta charset=\"UTF-8\" />\n" +
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
+                "        <title> Car Rental Service Backend Application </title>\n" +
+                "    </head>\n" +
+                "    <body>\n" +
+                "        <main>\n" +
+                "            <code>\n" +
+                "                \n" +
+                "                    {\n" +
+                "                        <pre>   \"message\": \"Service Available.\"</pre>\n" +
+                "                    }\n" +
+                "                </pre>\n" +
+                "            </code>\n" +
+                "        </main>\n" +
+                "    </body>\n" +
+                "</html>";
+    }
+}

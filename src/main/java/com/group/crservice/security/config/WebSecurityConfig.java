1be00cb734455862dc2689851a23df20d4de2578
midibi-admin <midibi.org@gmail.com>
2022-08-20 13:00:05 +0100

    Further edit

--- src/main/java/com/group/crservice/security/config/WebSecurityConfig.java
+++ src/main/java/com/group/crservice/security/config/WebSecurityConfig.java
@@ -87,7 +87,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "Cache-Control", "Content-Type", "client-key"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
